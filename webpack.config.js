d29853890ee73aa7fa9ac02dfc6f2f6768c9b371
dev-acc-practice <development-practice@proton.me>
2022-12-09 03:09:25 +0200

    installing webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+};
